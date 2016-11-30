6cf2c609368521fb9817c55018a959a5552b3cc4
Jay Yun <jay.yun@sendbird.com>
2016-12-01 03:23:38 +0900

    Update to 3.0.9.

--- app/src/main/java/com/sendbird/android/sample/MainActivity.java
+++ app/src/main/java/com/sendbird/android/sample/MainActivity.java
@@ -21,7 +21,7 @@ import com.sendbird.android.User;
  * SendBird Android Sample UI
  */
 public class MainActivity extends FragmentActivity {
-    public static String VERSION = "3.0.8.0";
+    public static String VERSION = "3.0.9.0";
 
     private enum State {DISCONNECTED, CONNECTING, CONNECTED}
 
